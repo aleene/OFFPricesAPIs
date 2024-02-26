9842a0530e1bbd12cc583afff42f50844c30d614
Arnaud Leene <aleene@gmail.com>
2024-02-26 15:54:46 +0100

    Initial Commit

---
+++ OFFPricesAPI/OFFPricesAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OFFPricesAPIApp.swift
+//  OFFPricesAPI
+//
+//  Created by Arnaud Leene on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct OFFPricesAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
