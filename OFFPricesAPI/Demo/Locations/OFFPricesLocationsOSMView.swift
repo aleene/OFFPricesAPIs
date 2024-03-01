32e9d374810cebfde0fa2ea37c62cd00ada1fe99
Arnaud Leene <aleene@gmail.com>
2024-03-01 09:22:29 +0100

    second locations endpoint

---
+++ OFFPricesAPI/Demo/Locations/OFFPricesLocationsOSMView.swift
@@ -0,0 +1,18 @@
+//
+//  OFFPricesLocationsOSMView.swift
+//  OFFPricesAPI
+//
+//  Created by Arnaud Leene on 29/02/2024.
+//
+
+import SwiftUI
+
+struct OFFPricesLocationsOSMView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OFFPricesLocationsOSMView()
+}
